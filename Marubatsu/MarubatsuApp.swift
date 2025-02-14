e51a4180ec77f688987ede6b488a7e023e713003
Tomonori <tomonori0204.takahashi@gmail.com>
2025-02-15 08:35:31 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by 高橋知憲 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
